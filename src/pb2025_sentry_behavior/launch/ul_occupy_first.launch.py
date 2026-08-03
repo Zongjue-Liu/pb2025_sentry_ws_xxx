d4660e8da1942d91ed191ed12849eb41e13f68e2
Zongjue-Liu <2215873441@qq.com>
2026-08-03 19:41:04 +0800

    feat(rmul): finalize three 2026 UL strategies

--- src/pb2025_sentry_behavior/launch/ul_occupy_first.launch.py
+++ src/pb2025_sentry_behavior/launch/ul_occupy_first.launch.py
@@ -1,3 +1,17 @@
+# Copyright 2026 SMBU PolarBear Robotics Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -6,15 +20,19 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                PathJoinSubstitution([
-                    FindPackageShare('pb2025_sentry_behavior'),
-                    'launch',
-                    'pb2025_sentry_behavior_launch.py',
-                ])
-            ),
-            launch_arguments={'target_tree': 'rmul2026_ul_occupy_first'}.items(),
-        )
-    ])
+    return LaunchDescription(
+        [
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    PathJoinSubstitution(
+                        [
+                            FindPackageShare("pb2025_sentry_behavior"),
+                            "launch",
+                            "pb2025_sentry_behavior_launch.py",
+                        ]
+                    )
+                ),
+                launch_arguments={"target_tree": "rmul2026_ul_occupy_first"}.items(),
+            )
+        ]
+    )
